8625dfb7794585d4e68b44a776f07fbf206e6c27
Mohammed Amin <mohammeda6429@hstat.org>
2018-10-10 03:30:03 +0000

    fix error

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,22 @@
-def division
-    
+def division(a, b)
+    a/b
 end
 
-def argue
-  
+def argue(c)
+    c
 end
 
-def greeting
-
-end
+def greeting(d, f)
+ end
 
 def return_a_value
-    
+    "Nice"
 end
 
-def pizza_party
-    
+def pizza_party(x="cheese")
+    x
 end
 
-def whisper
-    
+def whisper(y)
+    y.downcase
 end
